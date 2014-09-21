5cf94d52487a499478967840f9908259d1c022a3
FedericoMaggi <FedericoMaggi@users.noreply.github.com>
2014-09-21 16:23:06 +0200

    started implementation of moduleInstaller

---
+++ admin/modules/moduleinstaller/class/moduleinstaller.php
@@ -0,0 +1,24 @@
+<?
+	/********************************************/
+	/******* BestBug Studio Control Panel *******/
+	/********************************************/
+
+	define(MODULENAME, 'moduleinstaller');
+	define(BASEHREF, 'modules/'.MODULENAME.'/');
+
+Class ModuleInstaller{
+
+	public function getOptions(){
+
+	}
+	
+	public function getView($options){
+
+
+	}
+}
+
+	/********************************************/
+	/******* BestBug Studio Control Panel *******/
+	/********************************************/
+?>
---
+++ admin/modules/moduleinstaller/index.php
@@ -0,0 +1,12 @@
+<?
+	/********************************************/
+	/******* BestBug Studio Control Panel *******/
+	/********************************************/
+
+	include dirname(__FILE__).'/class/moduleinstaller.php';
+	return new ModuleInstaller();
+
+	/********************************************/
+	/******* BestBug Studio Control Panel *******/
+	/********************************************/
+?>
